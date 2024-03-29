fa76d710107228677933a46313dd15093b1ba45b
Rishikesh <rishikesh@obmondo.com>
2024-03-29 11:07:35 +0530

    WIP

--- config/config.go
+++ config/config.go
@@ -56,9 +56,9 @@ func (c *config) GetCVEAPITimeout() time.Duration {
 	cveTimeout := c.viper.GetInt("CVE_API_TIMEOUT")
 	if cveTimeout == 0 {
 		cveTimeout = c.viper.GetInt("cve_api.timeout")
-		if cveTimeout == 0 {
-			logger.GetLogger().Fatal(context.Background(), "failed to find timeout paramter for cve api in config and in environment", nil, nil)
-		}
+	}
+	if cveTimeout == 0 {
+		logger.GetLogger().Fatal(context.Background(), "failed to find timeout parameter for CVE API in config and in environment", nil, nil)
 	}
 
 	return time.Duration(cveTimeout) * time.Second
@@ -68,9 +68,10 @@ func (c *config) GetCVERequestInterval() time.Duration {
 	cveRequestInterval := c.viper.GetInt("CVE_API_REQUEST_INTERVAL")
 	if cveRequestInterval == 0 {
 		cveRequestInterval = c.viper.GetInt("cve_api.request_interval")
-		if cveRequestInterval == 0 {
-			logger.GetLogger().Fatal(context.Background(), "failed to find timeout paramter for cve api in config and in environment", nil, nil)
-		}
+	}
+
+	if cveRequestInterval == 0 {
+		logger.GetLogger().Fatal(context.Background(), "failed to find timeout paramter for cve api in config and in environment", nil, nil)
 	}
 
 	return time.Second * time.Duration(cveRequestInterval)
@@ -80,9 +81,10 @@ func (c *config) GetCVEAPI() string {
 	cveAPI := c.viper.GetString("CVE_API_URL")
 	if len(cveAPI) == 0 {
 		cveAPI = c.viper.GetString("cve_api.url")
-		if len(cveAPI) == 0 {
-			logger.GetLogger().Fatal(context.Background(), "failed to find cve_api value in env and in config file", nil, nil)
-		}
+	}
+
+	if len(cveAPI) == 0 {
+		logger.GetLogger().Fatal(context.Background(), "failed to find cve_api value in env and in config file", nil, nil)
 	}
 
 	return cveAPI
@@ -93,9 +95,10 @@ func (c *config) GetCVEAPIEndpoint() string {
 	cveAPIEndpoint := c.viper.GetString("CVE_API_ENDPOINT")
 	if len(cveAPIEndpoint) == 0 {
 		cveAPIEndpoint = c.viper.GetString("cve_api.endpoint")
-		if len(cveAPIEndpoint) == 0 {
-			logger.GetLogger().Fatal(context.Background(), "failed to find cve api endpoint in config and in environment", nil, nil)
-		}
+	}
+
+	if len(cveAPIEndpoint) == 0 {
+		logger.GetLogger().Fatal(context.Background(), "failed to find cve api endpoint in config and in environment", nil, nil)
 	}
 	return cveAPIEndpoint
 }
@@ -104,9 +107,11 @@ func (c *config) GetCVECronExpression() string {
 	cveCronExpression := c.viper.GetString("CRON_EXPRESSION")
 	if len(cveCronExpression) == 0 {
 		cveCronExpression = c.viper.GetString("cron_expression")
-		if len(cveCronExpression) == 0 {
-			logger.GetLogger().Fatal(context.Background(), "failed to find cron expression in config and in environment", nil, nil)
-		}
+
+	}
+
+	if len(cveCronExpression) == 0 {
+		logger.GetLogger().Fatal(context.Background(), "failed to find cron expression in config and in environment", nil, nil)
 	}
 	return cveCronExpression
 }
